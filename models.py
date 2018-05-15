5375935c135e7c7fc3e4cb99dd5a7a893e8608a3
Cameron Lai <cameronlai@users.noreply.github.com>
2018-05-16 00:29:20 +0800

    Fix time display problem

--- models.py
+++ models.py
@@ -1,4 +1,7 @@
 from django.db import models
+import time
+
+
 
 class AthResults(models.Model):
     id = models.IntegerField(primary_key=True, blank=True)  # AutoField?
@@ -10,33 +13,25 @@ class AthResults(models.Model):
     date = models.TextField(db_column='Date', blank=True)  # Field name made lowercase. This field type is a guess.
     track = models.IntegerField(db_column='Track', blank=True, null=True)  # Field name made lowercase.
 
+    def display_result(self):
+        if self.track:
+            m, s = divmod(self.result, 60)
+            h, m = divmod(m, 60)
+            if h > 0:
+                ret = r'%d:%d:%.2f' % (h, m, s)
+            elif m > 0:
+                ret = r'%d:%.2f' % (m, s)
+            else:
+                ret = r'%.2f' % (s)
+            return ret + 's'
+        else:
+            return str(self.result) + 'm'
+
     class Meta:
         managed = False
         db_table = 'ath_results'
 
-    def get_list(self, school_choice, sex_choice, event_choice):
-        if school_choice: # Valid school name
-            school_athlete_list = self.objects.filter(school=school_choice).order_by('name')
-        else:
-            school_athlete_list = self.objects.none()
 
-        sex_list = school_athlete_list.order_by('-sex').values_list('sex', flat=True).distinct()    
-        event_list = school_athlete_list.order_by('event').values_list('event', flat=True).distinct()
-
-        # Get back name list
-        if len(sex_list) > 0 and not sex_choice in sex_list:
-            sex_choice = sex_list[0]
-
-        if len(event_list) > 0 and not event_choice in event_list:
-            event_choice = event_list[0]
-
-        if sex_choice and event_choice:
-            athlete_list = self.objects.filter(school=school_choice, event=event_choice, sex=sex_choice).order_by('name')
-        else:
-            athlete_list = self.objects.none()
-
-        name_list = athlete_list.values_list('name', flat=True).distinct()
-
-        return sex_list, event_list, name_list
         
 
+        
