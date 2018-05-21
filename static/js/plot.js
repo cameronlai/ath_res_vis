130667f6f061c0204f222f796a94653be1318d9e
Cameron Lai <cameronlai@users.noreply.github.com>
2018-05-21 23:54:21 +0800

    Fix tooltip label problem

--- static/js/plot.js
+++ static/js/plot.js
@@ -1,3 +1,24 @@
+var map_time_label = function(is_track, label, decimal) {
+  var unit = 'm';
+  var ret = label.toString();
+  if (is_track) {
+    unit = 's';
+    var s = 14;
+    var l = 5;
+    if (label < 60) {
+      s = 17;
+    }
+    else {
+      if (decimal > 0) {
+	l = l + 1 + decimal;
+      }
+    }
+    ret = new Date(label * 1000).toISOString().substr(s, l);
+  }
+  ret = ret.concat(unit);
+  return ret;
+}
+
 var plot_data = function(ctx, is_track, plot_x, plot_y) {
   chart_type = 'bar';
   if (plot_x.length == 0) {
@@ -21,24 +42,22 @@ var plot_data = function(ctx, is_track, plot_x, plot_y) {
       legend: {
         display: false,
       },
+      tooltips: {
+	enabled: true,
+	mode: 'single',
+	callbacks: {
+	  label: function(tooltipItems, data){
+	    var ret = map_time_label(is_track, tooltipItems.yLabel, 2);
+	    return ret;
+	  }
+	}
+      },
       scales: {
 	yAxes: [
 	  {
 	    ticks: {
 	      callback: function(label){
-		var unit = 'm';
-		var ret = label.toString();
-		if (is_track) {
-		  unit = 's';
-		  var s = 14;
-		  var l = 5;
-		  if (label < 60) {
-		    s = 17;
-		    l = 5;
-		  }
-		  ret = new Date(label * 1000).toISOString().substr(s, l);
-		}
-		ret = ret.concat(unit);
+		var ret = map_time_label(is_track, label, 0);
 		return ret;
 	      }
 	    }
